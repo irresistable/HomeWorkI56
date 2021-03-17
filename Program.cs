acb68302eb73ae5af64a4dd113001badaea833d7
Sanches <autoscript@gmail.com>
2021-03-17 17:48:20 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HomeWork_I56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string card;
+            Console.Write("\nEnter value to search for the card: ");
+            int value = Int32.Parse(Console.ReadLine());
+            new Values(value, out card);
+            Console.WriteLine(card);
+            Console.ReadKey();
+        }
+    }
+    public class Values
+    {
+        private string[] Cards = { "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace" };
+        public Values(int value, out string card)
+        {
+            int searchIndex = value - 6;
+            card = Cards[searchIndex];
+            return;
+        }
+    }
+}
